0119f73dd5616dc08691f35ef37d0aa1d562be13
Purav Parekh <purav98@gmail.com>
2020-08-27 22:27:50 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+window.sr = ScrollReveal();
+sr.reveal(".navbar", {
+	duration: 2000,
+	origin: "bottom",
+});
+
+sr.reveal(".jumbotron", {
+	duration: 2000,
+	origin: "top",
+	distance: "300px",
+});
+
+sr.reveal(".welcome", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "300px",
+});
+
+sr.reveal(".code", {
+	duration: 2000,
+	origin: "left",
+	distance: "300px",
+});
+sr.reveal(".bootstrap", {
+	duration: 2000,
+	origin: "top",
+	distance: "300px",
+});
+sr.reveal(".css", {
+	duration: 2000,
+	origin: "right",
+	distance: "300px",
+});
+
+sr.reveal(".about-left", {
+	duration: 2000,
+	origin: "left",
+	distance: "300px",
+});
+
+sr.reveal(".about-right img", {
+	duration: 2000,
+	origin: "right",
+	distance: "300px",
+});
+
+sr.reveal(".panda", {
+	duration: 2000,
+	origin: "left",
+	distance: "300px",
+});
+
+sr.reveal(".chicken", {
+	duration: 2000,
+	origin: "top",
+	distance: "300px",
+});
+sr.reveal(".poo", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "300px",
+});
+sr.reveal(".unicorn", {
+	duration: 2000,
+	origin: "right",
+	distance: "300px",
+});
